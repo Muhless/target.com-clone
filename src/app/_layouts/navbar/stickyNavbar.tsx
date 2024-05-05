aa58b3c25b8379d8b469131744b8c5681784a874
Muhless <nuryadi.muhta@gmail.com>
2024-05-05 10:14:29 +0700

    sticky navbar

--- src/app/_layouts/navbar/stickyNavbar.tsx
+++ src/app/_layouts/navbar/stickyNavbar.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import logo from "/public/images/target-logo-transparent.png"
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophoneAlt, faPeopleArrows, faPeopleGroup, faPerson, faPersonRifle, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons/faMicrophone";
 
 
 const StickyNavbar = () => {
@@ -13,16 +16,21 @@ const StickyNavbar = () => {
                     width={45}
                     height={10}
                 />
+                <Link href="/home"></Link>
             </div>
             <div className="font-bold text-black">
-                <a className="pr-11">Categories</a>
+                <Link href="/categories" className="pr-12">Categories</Link>
                 <a className="pr-12">Deals</a>
                 <a className="pr-12">New & Featured</a>
                 <a className="pr-12">Pickup & Delivery</a>
             </div>
-            <div className="">
-                <input className="active:border-none bg-gray-100 rounded-md text-gray-700 w-search h-11 items-center flex px-3 text-sm" type="text" placeholder="What can we help you find?" />
-
+            <div className="bg-gray-100 rounded-md  w-search h-11 items-center flex px-3">
+                <input className="bg-gray-100 w-search focus:outline-none text-gray-700 placeholder:text-gray-600  text-sm" type="text" placeholder="What can we help you find?" />
+                <a href=""><FontAwesomeIcon className="text-gray-500 mr-5 text-lg" icon={faMicrophoneAlt}></FontAwesomeIcon></a>
+                <a href=""><FontAwesomeIcon className="text-gray-500" icon={faSearch}></FontAwesomeIcon></a>
+            </div>
+            <div>
+            <a href=""><FontAwesomeIcon className="text-gray-500" icon={faPeopleGroup}>Sign In</FontAwesomeIcon></a>
             </div>
         </div>
     )
---
+++ src/app/categories/page.tsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+    return (
+        <div>
+            <h1>Ini kategori</h1>
+        </div>
+    )
+}
+
+export default Categories
